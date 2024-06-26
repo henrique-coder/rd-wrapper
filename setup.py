3ccb471b1ae04276486599a23996995bfc35afc4
Henrique <henriquemoreira10fk@gmail.com>
2024-06-26 12:26:28 -0300

    Removed 3.7 and 3.13.0-beta.2 Python checkers

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
@@ -33,5 +32,5 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
-    python_requires='>=3.7, <=3.12'
+    python_requires='>=3.8, <=3.12'
 )
